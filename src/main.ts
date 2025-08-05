3b33febb541898398e65cce6c0df2a06e431c410
TheNazXX <nz.shevchuk.2002@gmail.com>
2025-08-05 22:42:04 +0300

    fix/cors

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,28 @@
-// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
-  app.enableCors({
-    origin: ["https://speak-up-frontend.vercel.app/", "http://localhost:3000/"],           
-    credentials: true,                       
-    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
-    allowedHeaders: ['Content-Type','Authorization','Accept'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
-
-
-  app.setGlobalPrefix('api');
-
-  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Speak-up API')
     .setDescription('Learn English!')
     .setVersion('1.0')
     .addTag('API')
     .build();
+
+app.use(cors({
+    credentials: true,
+    origin: '*'
+  }))
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+
   const document = SwaggerModule.createDocument(app, config);
-
-
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(8080);
 }
